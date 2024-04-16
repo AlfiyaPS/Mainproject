968d3fee745a450bc7fdd28cb8e153401eb2f5e5
AlfiyaPS <alfiyaps2024a@mca.ajce.in>
2024-04-16 22:04:44 +0530

    commit2

--- staydemo/settings.py
+++ staydemo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*q7xi1vhsdw6v26ggsi*@4jvzam303+ub7rq%j!t24s%bosls1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
